ce0bd5ca4bf37a5f917ffcb133b964404e29bc22
JO-HyeonMuk <6477asd@gmail.com>
2026-03-12 09:16:47 +0900

    Create 직사각형 별찍기.cpp
    이중 for문으로 찍을순 있지만 O(n)2 이 되는 걸 막기위해 string 형을 사용

---
+++ 직사각형 별찍기.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+
+    string s;
+
+    s.append(a, '*');
+
+    for (int i = 0; i < b; ++i)
+    {
+        cout << s << endl;
+    }
+    return 0;
+
+}
