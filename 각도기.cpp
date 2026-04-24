815a37eb0bbfff0fb25e647a77e8c96cf597b137
JoHyeonMuk <6477asd@gmail.com>
2026-04-24 09:47:32 +0900

    Fix angle classification logic in 각도기.cpp

--- 각도기.cpp
+++ 각도기.cpp
@@ -5,16 +5,23 @@ using namespace std;
 
 int solution(int angle) {
     int answer = 0;
+
+    // 1. 예각: 0도보다 크고 90도보다 작은 경우
     if (0 < angle && angle < 90){
         answer = 1;
     }
+    // 2. 직각: 정확히 90도인 경우
     else if(angle == 90){
         answer = 2;
     }
+    // 3. 둔각: 90도보다 크고 180도보다 작은 경우
     else if(90 < angle && angle <180){
         answer = 3;
     }
-    else 
+    // 4. 평각: 위의 조건에 해당하지 않는 경우 (보통 문제 맥락상 180도인 경우)
+    else {
         answer = 4;
+    }
+    
     return answer;
 }
